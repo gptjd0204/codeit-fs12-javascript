22681082ba4cad605b7f65aee6c480dc538044e1
gptjd0204 <gptjd0204@naver.com>
2026-03-06 18:26:20 +0900

    first commit

---
+++ 02_interactive/1_dom_select/practice/phase1_starter/script.js
@@ -0,0 +1,31 @@
+// =============================================
+// Phase 1: DOM 선택 + 내용 변경
+// =============================================
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 미션 ① app-title 텍스트 바꾸기
+  // 힌트: document.getElementById('app-title')로 선택 후 .textContent 사용
+  // 여기에 코드를 작성하세요 ↓
+  const appTitle = document.getElementById("app-title");
+
+  appTitle.textContent = "나만의 Todo 앱";
+  // 미션 ② app-subtitle 텍스트 바꾸기
+  const appSubtitle = document.getElementById("app-subtitle");
+
+  appSubtitle.textContent = "오늘도 목표를 향해 화이팅 !";
+
+  // 미션 ③ remaining-count 숫자를 3으로 바꾸기
+
+  const remainingCount = document.getElementById("remaining-count");
+
+  remainingCount.textContent = 3;
+
+  // 미션 ④ todo-list 요소를 콘솔에 출력하기
+  // 힌트: console.log()를 사용하세요
+  const todoList = document.getElementById("todo-list");
+
+  //todoList 요소 포함 X
+  console.log(todoList.innerHTML);
+  //todoList 요소 포함 O
+  console.log(todoList.outerHTML);
+});
